f059b6986b17d2a12ae072dbcce6cee16d02cc42
Relz <relz0071@gmail.com>
2016-09-21 02:30:02 +0300

    Добавлена проверка корректного ввода числа (параметр value). При ошибке выводятся допустимые символы для заданной системы счисления

--- radix/main.cpp
+++ radix/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 #define MIN_NOTATION 2
 #define MAX_NOTATION 36
 
+const string & DIGITS = "0123456789ABCDEFGHIGKLMNOPQRSTUVWXYZ";
+
 //Проверка достаточности параметров запуска программы
 bool IsValidArgumentsCount(int argc)
 {
@@ -18,7 +20,41 @@ bool IsValidNotation(int notation)
 {
     return (notation >= MIN_NOTATION && notation <= MAX_NOTATION);
 }
-        
+
+//Проверка введенного числа на допустимые символы для указанной системы счисления
+bool IsValidValue(const string &value, unsigned int radix)
+{
+    for (unsigned int i = 0; i < value.length(); ++i)
+    {
+        bool found = false;
+        for (unsigned int j = 0; j < radix; ++j)
+        {
+            if (value[i] == DIGITS[j])
+            {
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//Вывод в консоль допустимых символов через запятую для указанной системы счисления
+void PrintPossibleRadixSymbols(unsigned int radix)
+{
+    for (unsigned int i = 0; i < radix; ++i)
+    {
+        cout << DIGITS[i];
+        if (i != radix - 1)
+        {
+            cout << ", ";
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     if (!IsValidArgumentsCount(argc))
@@ -52,6 +88,14 @@ int main(int argc, char* argv[])
 
     stringstream(srcNotationStr) >> srcNotation;
     stringstream(dstNotationStr) >> dstNotation;
+
+    if (!IsValidValue(valueStr, srcNotation))
+    {
+        cout << "Error: value is invalid. Possible symbols: ";
+        PrintPossibleRadixSymbols(srcNotation);
+        cout << "\n";
+        return 1;
+    }
 
     if (!IsValidNotation(srcNotation))
     {
