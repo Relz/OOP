63f3d274ce8b6b66f8e9334973e9ae438332b3c1
Relz <relz0071@gmail.com>
2016-09-26 00:05:54 +0300

    test.bat принимает входной параметр - имя программы для запуска

---
+++ radix/test.bat
@@ -0,0 +1,43 @@
+set PROGRAM="~%1"
+
+rem Проведем замену строки в пустом файле
+%PROGRAM% empty.txt %TEMP%\empty.txt "some string" "some another string"
+if %ERRORLEVEL% NEQ 1 goto err
+
+rem Проведем замену строки в файле, имеющем одну строку
+%PROGRAM% one-line.txt %TEMP%\one-line.txt hardly hard
+if %ERRORLEVEL% EQU 1 goto err
+fc.exe %TEMP%\one-line.txt one-line.txt
+if %ERRORLEVEL% EQU 0 goto err
+
+rem Проведем замену строки в файле, имеющем несколько строк
+%PROGRAM% multiline.txt %TEMP%\multiline.txt factories flowers
+if %ERRORLEVEL% EQU 1 goto err
+fc.exe %TEMP%\multiline.txt multiline.txt
+if %ERRORLEVEL% EQU 0 goto err
+
+rem Проведем замену строки в файле, имеющем одну строку, при этом имеются несколько одинаковых слов
+%PROGRAM% one-line-multiword.txt %TEMP%\one-line-multiword.txt six five
+if %ERRORLEVEL% EQU 1 goto err
+fc.exe %TEMP%\one-line-multiword.txt one-line-multiword.txt
+if %ERRORLEVEL% EQU 0 goto err
+
+rem Проведем замену строки в файле, имеющем несколько строк, при этом имеются несколько одинаковых слов
+%PROGRAM% multiline-multiword.txt %TEMP%\multiline-multiword.txt that it
+if %ERRORLEVEL% EQU 1 goto err                               
+fc.exe %TEMP%\multiline-multiword.txt multiline-multiword.txt
+if %ERRORLEVEL% EQU 0 goto err
+
+rem mama test
+%PROGRAM% mama.txt %TEMP%/mama.txt ma mama
+if %ERRORLEVEL% EQU 1 goto err
+fc.exe %TEMP%\mama.txt mama.txt
+if %ERRORLEVEL% EQU 0 goto err
+
+
+echo Program testing succeeded
+exit 0
+
+:err
+echo Program testing failed
+exit 1
