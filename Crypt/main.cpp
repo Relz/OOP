0eb788fac3809c31e6cd5de40cffd3b2f460b5a9
Relz <relz0071@gmail.com>
2016-10-09 02:28:59 +0300

    Вынесены максимальное и минимальное значение параметра key; Добавлена функция проверки параметра строки key на допустимость символов; Добавлена функция перевода символа в цифру; Добавлена функция перевода строки в число 0..255; Реализован перевод входного параметра key в число, проверяя, что оно лежит в диапазоне от 0 до 255.

--- Crypt/main.cpp
+++ Crypt/main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 const unsigned ARG_COUNT = 4;
 
+const unsigned KEY_MIN = 0;
+const unsigned KEY_MAX = 255;
+
 //Проверка достаточности параметров запуска программы
 bool IsValidArgumentsCount(int argc)
 {
@@ -47,6 +50,52 @@ bool IsValidInputOutputFiles(char *inputFileName, char *outputFileName, ifstream
     return true;
 }
 
+//Функция проверки параметра строки key на допустимость символов
+bool IsValidKeyStr(string keyStr)
+{
+    for (unsigned i = 0; i < keyStr.length(); ++i)
+    {
+        if (keyStr[i] < '0' || keyStr[i] > '9')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//Функция переводит символ в цифру
+unsigned char CharToDigit(char ch)
+{
+    unsigned char digit = 0;
+    if (ch >= '0' && ch <= '9')
+    {
+        digit = ch - '0';
+    }
+    return digit;
+}
+
+//Функция переводит строку в положительное целочисленное число от 0 до 255
+unsigned char StrToUnsignedChar(const string &str, bool &wasError)
+{
+    unsigned char result = 0;
+    for (unsigned i = 0; i < str.length(); ++i)
+    {
+        unsigned char digit = CharToDigit(str[i]);
+        if (result > (UCHAR_MAX - digit) / 10)
+        {
+            wasError = true;
+            return 0;
+        }
+        result = result * 10 + digit;
+    }
+    return result;
+}
+
+bool IsValidKey(unsigned char key)
+{
+    return key >= KEY_MIN && key <= KEY_MAX;
+}
+
 int main(int argc, char* argv[])
 {
     if (!IsValidArgumentsCount(argc))
@@ -61,6 +110,32 @@ int main(int argc, char* argv[])
     {
         return 1;
     }
+
+    bool wasError = false;
+    string keyStr = argv[3];
+    if (keyStr == "")
+    {
+        cout << "Error: key not specified" << "\n";
+        return 1;
+    }
+    if (!IsValidKeyStr(keyStr))
+    {
+        cout << "Error: invalid key. Supported values: " << KEY_MIN << ".." << KEY_MAX << "\n";
+        return 1;
+    }
+
+    unsigned char key = StrToUnsignedChar(keyStr, wasError);
+    if (wasError)
+    {
+        cout << "Error: overflow, maximum key value = " << UCHAR_MAX << "\n";
+        return 1;
+    }
+    if (!IsValidKey(key))
+    {
+        cout << "Error: invalid key. Supported values: " << KEY_MIN << ".." << KEY_MAX << "\n";
+        return 1;
+    }
+    cout << (int)key << "\n";
 
     return 0;
 }
