a922f6e28386852c0a75e184196d392eb6d1a78a
Relz <relz0071@gmail.com>
2016-10-19 10:32:45 +0300

    Вернул работоспособность тестируемой программы.

--- StlVector/main.cpp
+++ StlVector/main.cpp
@@ -8,6 +8,7 @@ int main()
 {
     cout << "Enter double numbers separated by spaces. Enter End of file symbol for exit. " << "\n";
     vector<double> numbers(istream_iterator<double>(cin), (istream_iterator<double>()));
+	ProcessVector(numbers);
 	sort(numbers.begin(), numbers.end(), sortGreater);
     copy(numbers.begin(), numbers.end(), ostream_iterator<double>(cout, ", "));
     return 0;
--- StlVector/stdafx.h
+++ StlVector/stdafx.h
@@ -2,5 +2,6 @@
 
 #include <vector>
 #include <algorithm>
+#include "vector.h"
 
 bool sortGreater(double a, double b);
