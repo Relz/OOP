683a76b5ec7f8be0d99b33023fcb2e526d620c80
Relz <relz0071@gmail.com>
2016-11-12 17:25:53 +0300

    Табы изменены на пробелы

--- HtmlDecode/HtmlDecode.cpp
+++ HtmlDecode/HtmlDecode.cpp
@@ -2,40 +2,40 @@
 
 namespace
 {
-	std::string HTML_ENTITIES[5][2] =
-	{
-		{ "&quot;", "\"" },
-		{ "&apos;", "’" },
-		{ "&lt;", "<" },
-		{ "&gt;", ">" },
-		{ "&amp;", "&" }
-	};
+    std::string HTML_ENTITIES[5][2] =
+    {
+        { "&quot;", "\"" },
+        { "&apos;", "’" },
+        { "&lt;", "<" },
+        { "&gt;", ">" },
+        { "&amp;", "&" }
+    };
 }
 
 std::string HtmlDecode(const std::string &text)
 {
-	std::string result;
+    std::string result;
 
-	for (size_t textIndex = 0; textIndex < text.length(); ++textIndex)
-	{
-		bool wasReplace = false;
+    for (size_t textIndex = 0; textIndex < text.length(); ++textIndex)
+    {
+        bool wasReplace = false;
 
-		if (text[textIndex] == '&')
-		{
-			for (size_t i = 0; i < 5; ++i)
-			{
-				if (text.find(HTML_ENTITIES[i][0].c_str(), textIndex, HTML_ENTITIES[i][0].length()) == textIndex)
-				{
-					result += HTML_ENTITIES[i][1];
-					textIndex += HTML_ENTITIES[i][0].length() - 1;
-					wasReplace = true;
-				}
-			}
-		}
-		if (!wasReplace)
-		{
-			result += text[textIndex];
-		}
-	}
-	return result;
+        if (text[textIndex] == '&')
+        {
+            for (size_t i = 0; i < 5; ++i)
+            {
+                if (text.find(HTML_ENTITIES[i][0].c_str(), textIndex, HTML_ENTITIES[i][0].length()) == textIndex)
+                {
+                    result += HTML_ENTITIES[i][1];
+                    textIndex += HTML_ENTITIES[i][0].length() - 1;
+                    wasReplace = true;
+                }
+            }
+        }
+        if (!wasReplace)
+        {
+            result += text[textIndex];
+        }
+    }
+    return result;
 }
