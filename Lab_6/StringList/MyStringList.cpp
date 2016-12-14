02e602c95fb3d73b6aa07b9ae2fb327c5647f670
Relz <relz0071@gmail.com>
2016-12-14 03:50:29 +0300

    Добавлена структура итератора для класса CMyStringList. Добавлены методы для дальнейшей организации реализации.

--- Lab_6/StringList/MyStringList.cpp
+++ Lab_6/StringList/MyStringList.cpp
@@ -3,3 +3,43 @@
 CMyStringList::CMyStringList()
 {
 }
+
+CMyStringList::iterator *CMyStringList::begin() const
+{
+    return m_begin;
+}
+
+CMyStringList::iterator *CMyStringList::end() const
+{
+    return m_end;
+}
+
+size_t CMyStringList::GetSize() const
+{
+    return m_size;
+}
+
+bool CMyStringList::IsEmpty() const
+{
+    return m_size == 0;
+}
+
+void CMyStringList::InsertFront(std::string const& str)
+{
+
+}
+
+void CMyStringList::InsertBack(std::string const& str)
+{
+
+}
+
+void CMyStringList::Insert(iterator *pos, std::string const& str)
+{
+
+}
+
+void CMyStringList::Erase(iterator *pos)
+{
+
+}
--- Lab_6/StringList/MyStringList.h
+++ Lab_6/StringList/MyStringList.h
@@ -1,8 +1,31 @@
 #pragma once
+#include "stdafx.h"
+
 class CMyStringList
 {
 public:
+    struct iterator
+    {
+        std::string value;
+        iterator *next = nullptr;
+        iterator *prev = nullptr;
+    };
+
     CMyStringList();
     ~CMyStringList() = default;
+
+    iterator *begin() const;
+    iterator *end() const;
+    size_t GetSize() const;
+    bool IsEmpty() const;
+
+    void InsertFront(std::string const& str);
+    void InsertBack(std::string const& str);
+    void Insert(iterator *pos, std::string const& str);
+    void Erase(iterator *pos);
+private:
+    size_t m_size = 0;
+    iterator *m_begin;
+    iterator *m_end;
 };
 
