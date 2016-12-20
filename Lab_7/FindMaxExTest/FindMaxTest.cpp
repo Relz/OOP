c89e1ca944a85566a41f0eac387ed12e760a26d4
Relz <relz0071@gmail.com>
2016-12-20 08:46:36 +0300

    Добавлены тесты, показывающие, что в процессе работы функции FindMax выходной параметр не изменяется при пустом входном векторе.

--- Lab_7/FindMaxExTest/FindMaxTest.cpp
+++ Lab_7/FindMaxExTest/FindMaxTest.cpp
@@ -3,6 +3,13 @@
 
 #include "../FindMaxEx/FindMax.h"
 
+const Athlete Ivanov = { "Ivanov Ivan Ivanovich", 220.5, 100.5 };
+const Athlete Petrov = { "Petrov Petr Petrovich", 150.8, 78.42 };
+const Athlete Jay = { "Jay John Jamson", 100.5, 50.25 };
+const Athlete Turchinov = { "Turchinov Nikolay Olegovich", 180.0, 50.20 };
+
+const std::vector<Athlete> athletes = { Ivanov, Petrov, Jay, Turchinov };
+
 BOOST_AUTO_TEST_SUITE(FindMax_function)
 
     BOOST_AUTO_TEST_CASE(cannot_find_in_empty_vector)
@@ -11,6 +18,15 @@ BOOST_AUTO_TEST_SUITE(FindMax_function)
         BOOST_CHECK(!FindMax({}, max, [](const auto lhs, const auto rhs) {
             return (lhs < rhs);
         }));
+        BOOST_CHECK(isnan(max));
+
+        Athlete lessHeightAthlete = Ivanov;
+        BOOST_CHECK(!FindMax({}, lessHeightAthlete, IsLessHeight));
+        AreEquals(lessHeightAthlete, Ivanov);
+
+        Athlete lessWeightAthlete = Ivanov;
+        BOOST_CHECK(!FindMax({}, lessWeightAthlete, IsLessWeight));
+        AreEquals(lessWeightAthlete, Ivanov);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
