e25b84ebfe248583d96e8ec53c45b2eec4f04290
Relz <relz0071@gmail.com>
2016-12-20 09:03:10 +0300

    Добавлен тест для нахождения самого высокого и самого тяжелого спортсмена.

--- Lab_7/FindMaxExTest/FindMaxTest.cpp
+++ Lab_7/FindMaxExTest/FindMaxTest.cpp
@@ -6,7 +6,7 @@
 const Athlete Ivanov = { "Ivanov Ivan Ivanovich", 220.5, 100.5 };
 const Athlete Petrov = { "Petrov Petr Petrovich", 150.8, 78.42 };
 const Athlete Jay = { "Jay John Jamson", 100.5, 50.25 };
-const Athlete Turchinov = { "Turchinov Nikolay Olegovich", 180.0, 50.20 };
+const Athlete Turchinov = { "Turchinov Nikolay Olegovich", 180.0, 100.51 };
 
 const std::vector<Athlete> athletes = { Ivanov, Petrov, Jay, Turchinov };
 
@@ -36,6 +36,14 @@ BOOST_AUTO_TEST_SUITE(FindMax_function)
             return (lhs < rhs);
         }));
         BOOST_CHECK_EQUAL(max, -1);
+
+        Athlete heightestAthlete;
+        BOOST_CHECK(FindMax(athletes, heightestAthlete, IsLessHeight));
+        AreEquals(heightestAthlete, Ivanov);
+
+        Athlete weightestAthlete;
+        BOOST_CHECK(FindMax(athletes, weightestAthlete, IsLessWeight));
+        AreEquals(weightestAthlete, Turchinov);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
